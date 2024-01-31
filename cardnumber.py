3b8044129b58e1e7e713b19b76b54edeb549db7d
Elona Gorter <111061455+ilonka011@users.noreply.github.com>
2024-01-31 16:12:44 -0500

    Update cardnumber.py

--- cardnumber.py
+++ cardnumber.py
@@ -3,4 +3,4 @@ card_translation = str.maketrans({'-' : '', ' ' : ''})
 translated_card_number = card_number.translate(card_translation)
 reversed_card_number = translated_card_number[::-1]
 print(translated_card_number)
-print(reversed_card_number)
+
